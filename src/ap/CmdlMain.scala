54ab6cd217f13fd7042b4c1048125f0f72973584
Philipp Ruemmer <ph_r@gmx.net>
2012-04-21 10:36:47 +0000

    git-svn-id: svn://hal4.it.uu.se/princess/interpolation/trunk@458 bab55e88-d905-4164-9538-f3d1904550f0

--- src/ap/CmdlMain.scala
+++ src/ap/CmdlMain.scala
@@ -165,7 +165,7 @@ object CmdlMain {
         else if (filename endsWith ".p")
           Param.InputFormat.TPTP
         else
-          throw new Exception ("could not figure out the input format (recognised types: .pri, .smt2)")
+          throw new Exception ("could not figure out the input format (recognised types: .pri, .smt2, .p)")
       case f => f
   }
   
