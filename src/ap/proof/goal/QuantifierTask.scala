fd5bdcdd84a328e9c1532e9e8b69756c17c561bc
Philipp Ruemmer <ph_r@gmx.net>
2013-07-11 16:19:15 +0000

    tiny change that improves performance considerably
    git-svn-id: svn://hal4.it.uu.se/princess/interpolation/trunk@1098 bab55e88-d905-4164-9538-f3d1904550f0

--- src/ap/proof/goal/QuantifierTask.scala
+++ src/ap/proof/goal/QuantifierTask.scala
@@ -65,7 +65,7 @@ abstract class QuantifierTask(_formula : Conjunction, _age : Int)
     val constants = for (i <- PlainRange(num))
                     yield new ConstantTerm (constantNameBase + goal.age + "_" + i)
 
-    val newOrder = goal.order extend constants
+    val newOrder = goal.order extend constants.reverse
     
     val newBindingContext = goal.bindingContext.addAndContract(constants, quan)
     val newConstantFreedom = if (quan == Quantifier.ALL)
