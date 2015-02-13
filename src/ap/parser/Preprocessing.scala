9a1c9e205b4ca14a043cb65ba058acef64ad3b95
Philipp Ruemmer <ph_r@gmx.net>
2015-02-13 19:19:25 +0000

    removed debugging output
    git-svn-id: svn://hal4.it.uu.se/princess/interpolation/branches/unification-modulo@2046 bab55e88-d905-4164-9538-f3d1904550f0

--- src/ap/parser/Preprocessing.scala
+++ src/ap/parser/Preprocessing.scala
@@ -125,7 +125,6 @@ object Preprocessing {
       case Param.ClausifierOptions.None =>
         fors5
       case Param.ClausifierOptions.Simple =>
-      println(Param.CLAUSIFIER_TIMEOUT(settings))
         Timeout.withTimeoutMillis(Param.CLAUSIFIER_TIMEOUT(settings))(
           for (f <- fors5) yield (new SimpleClausifier)(f).asInstanceOf[INamedPart]
         )(throw new CmdlMain.GaveUpException("Clausification timed out"))
