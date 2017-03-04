789f928ca44fd47a1eb6b2e9bc5ec7d59d74ebbf
Philipp Ruemmer <ph_r@gmx.net>
2017-03-04 21:03:07 +0000

    started
    git-svn-id: svn://hal4.it.uu.se/princess/interpolation/branches/types@2738 bab55e88-d905-4164-9538-f3d1904550f0

---
+++ src/ap/types/Sort.scala
@@ -0,0 +1,35 @@
+/**
+ * This file is part of Princess, a theorem prover for Presburger
+ * arithmetic with uninterpreted predicates.
+ * <http://www.philipp.ruemmer.org/princess.shtml>
+ *
+ * Copyright (C) 2017 Philipp Ruemmer <ph_r@gmx.net>
+ *
+ * Princess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Princess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Princess.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ap.types
+
+import ap.parser._
+import ap.terfor.{Term, Formula}
+
+object Sort {
+
+}
+
+trait Sort {
+  val name : String
+  def membershipConstraint(t : ITerm) : IFormula
+  def membershipConstraint(t : Term) : Formula
+}
