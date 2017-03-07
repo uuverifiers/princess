0a70e9ae007b1f61e2de65c11c29e1239539798b
Philipp Ruemmer <ph_r@gmx.net>
2017-03-07 12:13:37 +0000

    git-svn-id: svn://hal4.it.uu.se/princess/interpolation/branches/types@2742 bab55e88-d905-4164-9538-f3d1904550f0

---
+++ src/ap/types/SortedSymbols.scala
@@ -0,0 +1,34 @@
+/**
+ * This file is part of Princess, a theorem prover for Presburger
+ * arithmetic with uninterpreted predicates.
+ * <http://www.philipp.ruemmer.org/princess.shtml>
+ *
+ * Copyright (C) 2017 Philipp Ruemmer <ph_r@gmx.net>
+ *
+ * Princess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Princess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Princess.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ap.types
+
+import ap.parser.IFunction
+import ap.terfor.ConstantTerm
+import ap.terfor.preds.Predicate
+
+/**
+ * Sorted version of constants.
+ */
+class SortedConstantTerm(_name : String, val sort : Sort)
+      extends ConstantTerm(_name)
+
+////////////////////////////////////////////////////////////////////////////////
