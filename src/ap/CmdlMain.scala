f5482ad16a1537ca0b11f02607c127eeb0f6c3a3
Philipp Ruemmer <ph_r@gmx.net>
2018-05-24 20:13:55 +0000

    release
    git-svn-id: svn://hal4.it.uu.se/princess/interpolation/tags/snapshot-2018-05-25@3153 bab55e88-d905-4164-9538-f3d1904550f0

--- src/ap/CmdlMain.scala
+++ src/ap/CmdlMain.scala
@@ -38,7 +38,7 @@ import ap.util.{Debug, Seqs, Timeout}
 
 object CmdlMain {
 
-  val version = "unstable build"
+  val version = "2018-05-25"
 
   def printGreeting = {
     println("________       _____")                                 
