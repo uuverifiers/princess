43bf274dc8f27bf9bdb3b26c626cbaccd9150b83
Philipp Ruemmer <ph_r@gmx.net>
2018-07-02 21:47:25 +0000

    comment
    git-svn-id: svn://hal4.it.uu.se/princess/interpolation/trunk@3210 bab55e88-d905-4164-9538-f3d1904550f0

--- src/ap/proof/ModelSearchProver.scala
+++ src/ap/proof/ModelSearchProver.scala
@@ -3,7 +3,7 @@
  * arithmetic with uninterpreted predicates.
  * <http://www.philipp.ruemmer.org/princess.shtml>
  *
- * Copyright (C) 2009-2017 Philipp Ruemmer <ph_r@gmx.net>
+ * Copyright (C) 2009-2018 Philipp Ruemmer <ph_r@gmx.net>
  *
  * Princess is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -955,6 +955,7 @@ class ModelSearchProver(defaultSettings : GoalSettings) {
       var (resGoal, additionalCertFormulas) = newOrderGoal addTasksFor fors
 
       // apply the most simple tasks right away
+      // TODO: do this also for FactsNormalisationTask, UpdateTasksTask
       var cont = true
       while (cont && resGoal.stepPossible) {
         cont = resGoal.tasks.max match {
