44a3ec3c6fe7607ee1be5291bfa313dee212817a
Philipp Ruemmer <ph_r@gmx.net>
2018-08-08 14:07:56 +0000

    missing file
    git-svn-id: svn://hal4.it.uu.se/princess/interpolation/trunk@3223 bab55e88-d905-4164-9538-f3d1904550f0

---
+++ src/ap/util/IndexedSeqView.scala
@@ -0,0 +1,33 @@
+/**
+ * This file is part of Princess, a theorem prover for Presburger
+ * arithmetic with uninterpreted predicates.
+ * <http://www.philipp.ruemmer.org/princess.shtml>
+ *
+ * Copyright (C) 2018 Philipp Ruemmer <ph_r@gmx.net>
+ *
+ * Princess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Princess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Princess.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ap.util;
+
+class IndexedSeqView[A](seq : IndexedSeq[A], begin : Int, end : Int)
+      extends scala.collection.immutable.IndexedSeq[A] {
+
+  def apply(n : Int) = seq(n + begin)
+
+  def length = end - begin
+
+  override def toIndexedSeq : scala.collection.immutable.IndexedSeq[A] = this
+
+}
