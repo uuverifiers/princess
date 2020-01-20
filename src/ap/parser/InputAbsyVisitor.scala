7d9ca331abbd0e1061195a28e19e2b99f9a20103
Philipp Ruemmer <ph_r@gmx.net>
2020-01-20 11:54:03 +0000

    handle triggers in prenex transformer
    git-svn-id: svn://hal4.it.uu.se/princess/interpolation/trunk@3691 bab55e88-d905-4164-9538-f3d1904550f0

--- src/ap/parser/InputAbsyVisitor.scala
+++ src/ap/parser/InputAbsyVisitor.scala
@@ -3,7 +3,7 @@
  * arithmetic with uninterpreted predicates.
  * <http://www.philipp.ruemmer.org/princess.shtml>
  *
- * Copyright (C) 2009-2018 Philipp Ruemmer <ph_r@gmx.net>
+ * Copyright (C) 2009-2020 Philipp Ruemmer <ph_r@gmx.net>
  *
  * Princess is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -1256,6 +1256,10 @@ class Transform2Prenex private (finalQuantifierNum : Int,
           VariableSubstVisitor(t, (context.a, finalQuantifierNum - context.a.size)))
       }
     }
+    // Triggers will not make sense anymore after turning to prenex, so
+    // currently we just drop them
+    case ITrigger(_, f) =>
+      TryAgain(f, context)
     case _ : IEpsilon => {
       //-BEGIN-ASSERTION-///////////////////////////////////////////////////////
       // case that we don't expect here
