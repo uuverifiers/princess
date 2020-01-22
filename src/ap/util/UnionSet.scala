cb9772dbc97521f30fcab7b851f83066112b10aa
Philipp Ruemmer <ph_r@gmx.net>
2020-01-22 13:23:39 +0000

    optimised UnionSet class
    git-svn-id: svn://hal4.it.uu.se/princess/interpolation/trunk@3692 bab55e88-d905-4164-9538-f3d1904550f0

--- src/ap/util/UnionSet.scala
+++ src/ap/util/UnionSet.scala
@@ -3,7 +3,7 @@
  * arithmetic with uninterpreted predicates.
  * <http://www.philipp.ruemmer.org/princess.shtml>
  *
- * Copyright (C) 2009-2014 Philipp Ruemmer <ph_r@gmx.net>
+ * Copyright (C) 2009-2020 Philipp Ruemmer <ph_r@gmx.net>
  *
  * Princess is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -21,9 +21,13 @@
 
 package ap.util;
 
+import scala.collection.mutable.{HashSet => MHashSet}
+
 object UnionSet {
   
   private val AC = Debug.AC_SET_UTILS
+
+  private val ACCESS_CNT_THRESHOLD = 100
 
   def apply[A](set1 : scala.collection.Set[A],
                set2 : scala.collection.Set[A]) : scala.collection.Set[A] =
@@ -33,6 +37,13 @@ object UnionSet {
       set1
     else */
       new UnionSet (set1, set2)
+
+  def maybeDuplicateIterator[A](that : scala.collection.Set[A]) = that match {
+    case that : UnionSet[A] =>
+      that.maybeDuplicateIterator
+    case that =>
+      that.iterator
+  }
 }
 
 /**
@@ -43,21 +54,67 @@ class UnionSet[A] private (set1 : scala.collection.Set[A],
                            set2 : scala.collection.Set[A])
       extends scala.collection.Set[A] {
 
-  def contains(x : A) : Boolean = (set1 contains x) || (set2 contains x)
-  
-  override lazy val size : Int = {
-    var res = set1.size
-    for (x <- set2) {
-      if (!(set1 contains x))
-        res = res + 1
+  def contains(x : A) : Boolean =
+    localCopy match {
+      case null => {
+        maybeCopy
+        (set1 contains x) || (set2 contains x)
+      }
+      case copy =>
+        copy contains x
     }
-    res
-  }
+  
+  override lazy val size : Int =
+    localCopy match {
+      case null => {
+        var res = set1.size
+        for (x <- set2) {
+          if (!(set1 contains x))
+            res = res + 1
+        }
+        res
+      }
+      case copy =>
+        copy.size
+    }
   
   def iterator : Iterator[A] =
-    set1.iterator ++ FilterIt(set2.iterator, (x:A) => !(set1 contains x))
+    localCopy match {
+      case null => {
+        maybeCopy
+        set1.iterator ++ FilterIt(set2.iterator, (x:A) => !(set1 contains x))
+      }
+      case copy =>
+        copy.iterator
+    }
   
+  /**
+   * Iterator that might return some of the elements repeatedly
+   */
+  def maybeDuplicateIterator : Iterator[A] =
+    localCopy match {
+      case null =>
+        UnionSet.maybeDuplicateIterator(set1) ++
+          UnionSet.maybeDuplicateIterator(set2)
+      case copy =>
+        copy.iterator
+    }
+
   def +(elem: A) = throw new UnsupportedOperationException
   def -(elem: A) = throw new UnsupportedOperationException
+
+  private var accessCnt = 0
+
+  private var localCopy : MHashSet[A] = null
+
+  private def maybeCopy : Unit = {
+    accessCnt = accessCnt + 1
+    if (accessCnt > UnionSet.ACCESS_CNT_THRESHOLD) {
+      val copy = new MHashSet[A]
+      copy ++= UnionSet.maybeDuplicateIterator(set1)
+      copy ++= UnionSet.maybeDuplicateIterator(set2)
+      localCopy = copy
+    }
+  }
 
 }
