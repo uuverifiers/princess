a21a3cec786f41f6ee9ac9227a73051bfe47b213
Zafer Esen <zafer.esen@it.uu.se>
2020-03-09 15:00:38 +0000

    added preamble
    git-svn-id: svn://hal4.it.uu.se/princess/interpolation/branches/heap@3701 bab55e88-d905-4164-9538-f3d1904550f0

--- src/ap/theories/Heap.scala
+++ src/ap/theories/Heap.scala
@@ -1,3 +1,25 @@
+/**
+ * This file is part of Princess, a theorem prover for Presburger
+ * arithmetic with uninterpreted predicates.
+ * <http://www.philipp.ruemmer.org/princess.shtml>
+ *
+ * Copyright (C) 2016-2020 Philipp Ruemmer <ph_r@gmx.net>
+ *               2020      Zafer Esen <zafer.esen@gmail.com>
+ *
+ * Princess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Princess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Princess.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ap.theories
 
 import ap.basetypes._
