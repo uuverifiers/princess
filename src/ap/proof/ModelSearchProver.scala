5d5c5a6e2d81c931c77abf875f51e0dd31f166df
Philipp Ruemmer <ph_r@gmx.net>
2020-03-12 16:43:42 +0000

    fixed a bug related to incremental solving
    git-svn-id: svn://hal4.it.uu.se/princess/interpolation/trunk@3711 bab55e88-d905-4164-9538-f3d1904550f0

--- src/ap/proof/ModelSearchProver.scala
+++ src/ap/proof/ModelSearchProver.scala
@@ -3,7 +3,7 @@
  * arithmetic with uninterpreted predicates.
  * <http://www.philipp.ruemmer.org/princess.shtml>
  *
- * Copyright (C) 2009-2019 Philipp Ruemmer <ph_r@gmx.net>
+ * Copyright (C) 2009-2020 Philipp Ruemmer <ph_r@gmx.net>
  *
  * Princess is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -424,7 +424,13 @@ class ModelSearchProver(defaultSettings : GoalSettings) {
           findModel(uGoal,
                     List(), witnesses,
                     constsToIgnore, depth, settings, searchDirector,
-                    lemmaBase, lemmaBaseAssumedInferences)
+                    lemmaBase, lemmaBaseAssumedInferences) match {
+            case r@EFRerunResult(formulas) =>
+              EFRerunResult(extraFormulae ++ formulas)
+            case r@UnsatEFResult(formulas) =>
+              UnsatEFResult(extraFormulae ++ formulas)
+            case r => r
+          }
           
         } else {
 
