04affde57be2a0795f433cc7679e82a1e97c8bdb
Philipp Ruemmer <ph_r@gmx.net>
2020-07-10 19:39:36 +0000

    corrected handling of sorts
    git-svn-id: svn://hal4.it.uu.se/princess/interpolation/trunk@3815 bab55e88-d905-4164-9538-f3d1904550f0

--- src/ap/parser/Simplifier.scala
+++ src/ap/parser/Simplifier.scala
@@ -3,7 +3,7 @@
  * arithmetic with uninterpreted predicates.
  * <http://www.philipp.ruemmer.org/princess.shtml>
  *
- * Copyright (C) 2009-2019 Philipp Ruemmer <ph_r@gmx.net>
+ * Copyright (C) 2009-2020 Philipp Ruemmer <ph_r@gmx.net>
  *
  * Princess is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -32,7 +32,9 @@ import Quantifier._
 import scala.collection.mutable.ArrayBuffer
 
 /**
- * Class to simplify input formulas using various rewritings
+ * Class to simplify input formulas using various rewritings.
+ * Argument <code>splittingLimit</code> controls whether
+ * the formula is also (naively) turned into DNF.
  */
 class Simplifier(splittingLimit : Int = 20,
                  doMiniScoping : Boolean = true) {
