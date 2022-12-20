e2602c916c4f59f763a6005953c39c5f05b1e1ed
Philipp Ruemmer <ph_r@gmx.net>
2022-12-20 13:01:37 +0100

    started encoding arrays over complex numbers

---
+++ testcases/arraysAPI/ComplexTest.scala
@@ -0,0 +1,135 @@
+
+import ap.SimpleAPI
+import ap.parser._
+import ap.basetypes.IdealInt
+import ap.theories.ADT
+import ADT.BoolADT.{True, False}
+import ap.theories.arrays._
+
+object ComplexTest extends App {
+
+  val debug = true
+
+  ap.util.Debug.enableAllAssertions(debug)
+
+  import IExpression._
+
+  val (complexSort, complex, Seq(a, b, c, d, k)) =
+    ADT.createRecordType("Complex", List(("a", Sort.Integer),
+                                         ("b", Sort.Integer),
+                                         ("c", Sort.Integer),
+                                         ("d", Sort.Integer),
+                                         ("k", Sort.Nat)))
+
+  def complexPlus(s : ITerm, t : ITerm) : ITerm =
+    ite(k(s) === k(t),
+        complex(a(s) + a(t), b(s) + b(t), c(s) + c(t), d(s) + d(t), k(s)),
+        complex(0, 0, 0, 0, 0))
+
+  def omegaMult(s : ITerm) : ITerm =
+    complex(-d(s), a(s), b(s), c(s), k(s))
+
+  def intMult(s : ITerm, n : IdealInt) : ITerm =
+    complex(a(s) * n, b(s) * n, c(s) * n, d(s) * n, k(s))
+
+  def sqrt2Div(s : ITerm) : ITerm =
+    complex(a(s), b(s), c(s), d(s), k(s) + 1)
+
+  def kInc2(s : ITerm) : ITerm =
+    complex(a(s) * 2, b(s) * 2, c(s) * 2, d(s) * 2, k(s) + 2)
+
+  def setK1(s : ITerm) : ITerm =
+    complex(a(s), b(s), c(s), d(s), 1)
+
+  val N = 10
+
+  val vectorOps = Vector(
+    CombArray.CombinatorSpec("vec_plus", List(0, 0), 0,
+                             v(2, complexSort) === complexPlus(v(0, complexSort), v(1, complexSort))),
+    CombArray.CombinatorSpec("vec_omegaMult", List(0), 0,
+                             v(1, complexSort) === omegaMult(v(0, complexSort))),
+    CombArray.CombinatorSpec("vec_negate", List(0), 0,
+                             v(1, complexSort) === intMult(v(0, complexSort), -1)),
+    CombArray.CombinatorSpec("vec_sqrt2Div", List(0), 0,
+                             v(1, complexSort) === sqrt2Div(v(0, complexSort))),
+    CombArray.CombinatorSpec("vec_setK1", List(0), 0,
+                             v(1, complexSort) === setK1(v(0, complexSort)))
+  )
+
+  def bools(n : Int) = for (_ <- 0 until n) yield Sort.Bool
+
+  val CartTheory =
+    new CartArray(bools(N), complexSort, 1, vectorOps)
+  
+  val arrayN  = CartTheory.extTheories(bools(N))
+  val arrayN1 = CartTheory.extTheories(bools(N - 1))
+
+  def projN(k : Int)  = CartTheory.projections((bools(N), k))
+
+  val arrayNComb = CartTheory.combTheories(bools(N))
+  val Seq(vec_plusN, vec_omegaMultN, vec_negateN, vec_sqrt2DivN, vec_setK1N) = arrayNComb.combinators
+
+  val arrayN1Comb = CartTheory.combTheories(bools(N - 1))
+  val Seq(vec_plusN1, vec_omegaMultN1, vec_negateN1, vec_sqrt2DivN1, vec_setK1N1) = arrayN1Comb.combinators
+
+  def hadam(k : Int, x : ITerm, hx : ITerm) : IFormula =
+    arrayN1.sort.ex( (p0, p1) => {
+      p0 === projN(k)(x, False) & p1 === projN(k)(x, True) &
+      projN(k)(hx, False) === vec_sqrt2DivN1(vec_plusN1(p0, p1)) &
+      projN(k)(hx, True)  === vec_sqrt2DivN1(vec_plusN1(p0, vec_negateN1(p1)))
+    })
+
+  SimpleAPI.withProver(enableAssert = debug) { p =>
+    import p._
+
+    addTheory(CartTheory)
+
+    val x0 = createConstant("x0", arrayN.sort)
+    val x = createConstant("x", arrayN.sort)
+    val y = createConstant("y", arrayN.sort)
+    val z = createConstant("z", arrayN.sort)
+    val y1 = createConstant("y1", arrayN.sort)
+    val z1 = createConstant("z1", arrayN.sort)
+
+    val comp0 = createConstant("comp0", complexSort)
+    val comp1 = createConstant("comp1", complexSort)
+
+    scope {
+      !! (hadam(0, x, y))
+      !! (hadam(0, y, z))
+      !! (arrayN.select(x, False, False, False, False, False, False, False, False, False, False) ===
+            complex(1, 1, 1, 1, 1))
+      !! (arrayN.select(x, True,  False, False, False, False, False, False, False, False, False) ===
+            complex(2, 2, 2, 2, 1))
+      !! (arrayN.select(z, False, False, False, False, False, False, False, False, False, False) ===
+            comp0)
+      !! (arrayN.select(z, True, False, False, False, False, False, False, False, False, False) ===
+            comp1)
+      println(???) // sat
+      println(evalToTerm(comp0))
+      println(evalToTerm(comp1))
+    }
+
+    scope {
+      val Seq(b0, b1, b2, b3, b4, b5, b6, b7, b8, b9) = createConstants(N, Sort.Bool)
+
+      // Assert that all k-components are initially 1
+      !! (x === vec_setK1N(x0))
+
+      // Encoding of the circuit
+      !! (hadam(1, x, y))
+      !! (hadam(1, y, z))
+//      !! (hadam(2, z, y1))             // not working properly yet
+//      !! (hadam(2, y1, z1))
+
+      // Assertion: x, z coincide modulo normalization
+      ?? (kInc2(arrayN.select(x, b0, b1, b2, b3, b4, b5, b6, b7, b8, b9)) ===
+                arrayN.select(z, b0, b1, b2, b3, b4, b5, b6, b7, b8, b9))
+
+      println(???) // valid
+    }
+
+
+  }
+
+}
